ecb0d554507277a0ac1d86be775c78ce11a107d6
Malcolm Sparks <mal@juxt.pro>
2023-01-02 14:49:49 +0000

    Update swagger.json to more modern openapi.json

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://data.site.test/_site/swagger.json",
+    url: "https://data.site.test/_site/openapi.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
